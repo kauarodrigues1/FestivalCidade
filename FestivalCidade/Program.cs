6bea54fabc968ca9d5aeba274c0bb2c1acb2f12a
Kaua <rodrigueskaua76@gmail.com>
2026-08-24 22:16:41 -0300

    feat: program criado

--- FestivalCidade/Program.cs
+++ FestivalCidade/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FestivalCidade;
+
+Console.Write("Digite o nome do fã: ");
+string nome = Console.ReadLine() ?? "";
+
+Console.Write("Digite a quantidade de dias de show: ");
+int quantidadeDias = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Digite o cupom de desconto: ");
+string cupom = Console.ReadLine() ?? "";
+
+Console.Write("Digite a pontuação do clube de fidelidade: ");
+int pontuacaoClube = int.Parse(Console.ReadLine() ?? "0");
+
+Usuario usuario = new Usuario(
+    nome,
+    quantidadeDias,
+    cupom,
+    pontuacaoClube
+);
+
+Console.WriteLine();
+Console.WriteLine("===== FESTIVALCIDADE =====");
+Console.WriteLine($"Nome do Fã: {usuario.Nome.ToUpper()}");
+Console.WriteLine($"Valor Bruto: R$ {usuario.CalcularCustoBruto():F2}");
+Console.WriteLine($"Valor Final: R$ {usuario.CalcularValorFinal():F2}");
+Console.WriteLine($"Acesso ao Lounge VIP: {usuario.TemAcessoVip()}");
